ac17d4310a1b558a9cf853c1e787f5ab2e46aaa9
mohammad <oversycho41@gmail.com>
2026-07-14 11:54:33 +0330

    initializing app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vstore/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,27 +11,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final defaultTextStyle = TextStyle(
+      fontFamily: 'IranYekan',
+      color: LightThemeColors.primaryTextColor,
+    );
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Vision Store',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        textTheme: TextTheme(
+          bodyMedium: defaultTextStyle,
+          headlineSmall: defaultTextStyle.copyWith(fontWeight: FontWeight.w700),
+          labelSmall: defaultTextStyle
+              .copyWith(fontSize: 12, fontWeight: FontWeight.w700)
+              .apply(color: LightThemeColors.secondaryTextColor),
+        ),
+        colorScheme: ColorScheme.light(
+          primary: LightThemeColors.primaryColor,
+          secondary: LightThemeColors.secondaryColor,
+          onSecondary: Colors.white,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Directionality(
+        textDirection: TextDirection.rtl,
+        child: MyHomePage(title: 'فروشگاه'),
+      ),
     );
   }
 }
@@ -89,22 +93,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('کلیک کنید'),
+            Text('کلیک کنید', style: Theme.of(context).textTheme.labelSmall),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -113,6 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LightThemeColors {
+  static const primaryColor = Color(0xff007AFF);
+  static const secondaryColor = Color(0xff262a35);
+  static const primaryTextColor = Color(0xff262a35);
+  static const secondaryTextColor = Color.fromARGB(255, 127, 127, 129);
+}
