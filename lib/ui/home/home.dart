f5860fc1438227bfa2b4a88939b3d30aca80200a
mohammad <oversycho41@gmail.com>
2026-07-24 19:02:03 +0330

    initializing banners page view with indicators    iamge loading services banner ,dart slider .dart

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:vstore/data/repo/banner_repository.dart';
 import 'package:vstore/data/repo/product_repository.dart';
 import 'package:vstore/ui/home/bloc/home_bloc.dart';
+
+import 'package:vstore/ui/widgets/slider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -26,16 +29,21 @@ class HomeScreen extends StatelessWidget {
               if (state is HomeSuccess) {
                 return ListView.builder(
                   itemCount: 7,
-                  padding: EdgeInsets.fromLTRB(12, 12, 12, 100),
+
                   itemBuilder: (context, index) {
                     switch (index) {
                       case 0:
-                        return Image.asset(
-                          'assets/img/vstore_logo.png',
-                          height: 65,
+                        return Container(
+                          height: 56,
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            'assets/img/vstore_logo.png',
+                            fit: BoxFit.fitHeight,
+                            height: 50,
+                          ),
                         );
-                      case 1:
-                        return Container();
+                      case 2:
+                        return BannerSlider(banners: state.banners);
                       default:
                         return Container();
                     }
---
+++ lib/ui/widgets/slider.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:vstore/data/banner.dart';
+import 'package:vstore/ui/widgets/image.dart';
+
+class BannerSlider extends StatelessWidget {
+  final PageController _Controller = PageController();
+  final List<BannerEntity> banners;
+  BannerSlider({super.key, required this.banners});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2,
+      child: Stack(
+        children: [
+          PageView.builder(
+            controller: _Controller,
+            itemCount: banners.length,
+            itemBuilder: (context, index) => _Slide(banner: banners[index]),
+          ),
+
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 8,
+            child: Center(
+              child: SmoothPageIndicator(
+                controller: _Controller,
+                count: banners.length,
+                axisDirection: Axis.horizontal,
+                effect: WormEffect(
+                  spacing: 4.0,
+                  radius: 4.0,
+                  dotWidth: 20.0,
+                  dotHeight: 5.0,
+                  paintStyle: PaintingStyle.fill,
+                  strokeWidth: 1.5,
+                  dotColor: const Color.fromARGB(255, 150, 147, 147),
+                  activeDotColor: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final BannerEntity banner;
+  const _Slide({required this.banner});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsGeometry.only(left: 12, right: 12),
+      child: ImageLoadingService(
+        imageUrl: banner.imageUrl,
+        borderRadius: BorderRadius.circular(24),
+      ),
+    );
+  }
+}
